e1468a441f621726c4c19b02d148aaa94c9da411
derGrimnebulin <zftidwell@gmail.com>
2015-04-11 21:24:53 -0400

    Flushed out CellArray impl a bit more

--- src/main/scala/Structure/Cellery/CellArray.scala
+++ src/main/scala/Structure/Cellery/CellArray.scala
@@ -3,14 +3,61 @@ package Structure.Cellery
 import Structure.Cells.Cell
 
 trait CellArray {
+  
+  //type CellArray = Either[List[CellArray], List[Cell]]
+  /**
+   * the number of dimensions of this CellArray. Requires that dim is
+   * at least 1.
+   */
+  val dim: Int
 
-  val cells: Vector[_]
+  //Need to implement topology
+  //val topology: Topology
+
+  val cells: Either[List[CellArray], List[Cell]]
 
   /**
-   * Get the cell at the specified coordinates
-   * @param coord
+   *
+   * @param pos
    * @return
    */
-  def getCell(coord: Int*): Cell
+  def getCell(pos: List[Int]): Cell = {
+    require(pos.length == dim)
+
+    def collect(pos: List[Int], c: CellArray): Cell = c.cells match {
+      case Right(r) => r(pos.head)
+      case Left(l) => collect(pos.tail, l(pos.head))
+    }
+
+    collect(pos, this)
+  }
+
+  def neighbors(coord: Vector[Int]): Cell
+
+}
+
+/**
+ * CellArray Factory object
+ */
+object CellArray {
+
+  abstract class CellArray1D extends CellArray {
+    override val dim: Int = 1
+  }
+
+  abstract class CellArray2D extends CellArray {
+    override val dim: Int = 2
+  }
+
+  abstract class CellArray3D extends CellArray {
+    override val dim: Int = 3
+  }
+
+  /**
+   *
+   * @param cells
+   * @return
+   */
+  def apply(cells: Cell*): CellArray = ???
 
 }
